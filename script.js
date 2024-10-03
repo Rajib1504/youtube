acfa132db4c5c0782ebe4fa046964a65183a6667
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-10-03 13:15:14 +0530

    btn done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("add");
+function getData() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => display(data.categories));
+  function display(data) {
+    for (const value of data) {
+      // console.log(value);
+      const btn = document.createElement("button");
+      btn.classList = "btn border-2 border-red-600";
+      btn.innerText = value.category;
+      const catagory = document.getElementById("catagory");
+      catagory.append(btn);
+    }
+  }
+}
+getData();
