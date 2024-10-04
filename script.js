adf892bfe99b34ab775e55b3048441f77d7cd81b
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-10-04 18:27:07 +0530

    btn

--- script.js
+++ script.js
@@ -11,15 +11,17 @@ function getData() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
     .then((data) => display(data.categories));
-  function display(data) {
-    for (const value of data) {
-      // console.log(value);
-      const btn = document.createElement("button");
-      btn.classList = "btn border-2 border-red-600";
-      btn.innerText = value.category;
-      const catagory = document.getElementById("catagory");
-      catagory.append(btn);
-    }
+}
+function display(data) {
+  for (const value of data) {
+    // console.log(value);
+    const btn = document.createElement("button");
+    btn.classList = "btn border-2 border-red-600";
+    btn.id = "clicked";
+    btn.onclick = () => fetchVideoByCatagory(value.category_id);
+    btn.innerText = value.category;
+    const catagory = document.getElementById("catagory");
+    catagory.append(btn);
   }
 }
 getData();
@@ -29,47 +31,71 @@ const videosAddHere = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res) => res.json())
     .then((data) => printVideo(data.videos));
-
-  function printVideo(videos) {
-    const videoAdd = document.getElementById("video");
-    for (const video of videos) {
-      console.log(video);
-      const add = document.createElement("div");
-      add.innerHTML = `
+};
+function printVideo(videos) {
+  const videoAdd = document.getElementById("video");
+  videoAdd.innerHTML = "";
+  if (videos.length == 0) {
+    videoAdd.classList.remove("grid");
+    videoAdd.innerHTML = `<div class =" min-h-[300px] flex flex-col gap-5 justify-center items-center">
+    <img src="./assets/icon.png" >
+    <h2 class ="font-bold text-xl text-center">No content here</h2>
+    </div>`;
+  } else {
+    videoAdd.classList.add("grid");
+  }
+  for (const video of videos) {
+    // console.log(video);
+    const add = document.createElement("div");
+    add.innerHTML = `
 <div class="flex flex-col gap-2">
-              <figure class ="h-[200px] relative">
-          <img
-            src="${video.thumbnail}"class ="object-cover h-full w-full pb-2"
-            alt="" />
-            ${
-              video.others.posted_date?.length == 0
-                ? ""
-                : `<span class ="absolute text-xs right-1 bottom-3 bg-black rounded-sm text-white">${getTime(
-                    video.others.posted_date
-                  )}</span>`
-            }     
-        </figure>
+            <figure class ="h-[200px] relative">
+        <img
+          src="${video.thumbnail}"class ="object-cover h-full w-full pb-2"
+          alt="" />
+          ${
+            video.others.posted_date?.length == 0
+              ? ""
+              : `<span class ="absolute text-xs right-1 bottom-3 bg-black rounded-sm text-white">${getTime(
+                  video.others.posted_date
+                )}</span>`
+          }     
+      </figure>
 <div class =" flex gap-2 justify-start ">
 <img class="rounded-full object-cover w-10 h-10" src ="${
-        video.authors[0].profile_picture
-      }  alt ="" />
-        <div class="card-body p-0">
-          <h2 class="card-title font-bold">${video.title}</h2>
-          <div class="flex justify-evenly  items-center gap-2"
-           <p class = "text-gray-400">${video.authors[0].profile_name}</p>
-           ${
-             video.authors[0].verified === true
-               ? `<img class ="w-6" src ="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png"/>`
-               : ""
-           } 
+      video.authors[0].profile_picture
+    }  alt ="" />
+      <div class="card-body p-0">
+        <h2 class="card-title font-bold">${video.title}</h2>
+        <div class="flex justify-evenly  items-center gap-2"
+         <p class = "text-gray-400">${video.authors[0].profile_name}</p>
+         ${
+           video.authors[0].verified === true
+             ? `<img class ="w-6" src ="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png"/>`
+             : ""
+         } 
 </div>
-      <p>views : ${video.others.views}</p>
-      </div>
-      </div>
-      </div>
-        `;
-      videoAdd.append(add);
-    }
+    <p>views : ${video.others.views}</p>
+    </div>
+    </div>
+    </div>
+      `;
+    videoAdd.append(add);
   }
-};
+}
 videosAddHere();
+
+// pop up section :
+const fetchVideoByCatagory = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => printVideo(data.category));
+};
+
+// const activeBtns = document.getElementsByClassName("activeBtn");
+// for (const btn of activeBtns) {
+//   console.log(btn);
+//   btn.addEventListener("click", () => {
+//     console.log("clicked");
+//   });
+// }
--- test.js
+++ test.js
@@ -1,12 +1,19 @@
 function getTime(time) {
-  let hour = parseInt(time / 3600);
-  let remainingsec = time % 3600;
+  let year = parseInt(time / 3104000);
+  let remainingsec = time % 3104000;
+  console.log(remainingsec);
+  let month = parseInt(remainingsec / 2592000);
+  remainingsec = remainingsec % 2592000;
+  let day = parseInt(remainingsec / 86400);
+  remainingsec = remainingsec % 86400;
+  let hour = parseInt(remainingsec / 3600);
+  remainingsec = remainingsec % 3600;
   //   console.log(remainingsec);
   let min = parseInt(remainingsec / 60);
   remainingsec = remainingsec % 60;
-  return `${hour}hour ${min} min ${remainingsec} sec ago`;
+  return `${year}year${month}month${day}day${hour}hour ${min} min ${remainingsec} sec ago`;
 }
 
-const time = getTime(16278);
-
+// const time = getTime(16278);
+const time = getTime(1672656000);
 console.log(time);
